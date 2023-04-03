06e13f9450204c1c5eaab476e5f75e58a428c076
open-meadow <rohan.varughese@protonmail.com>
2023-04-03 15:31:13 -0400

    Verify that the code is working

--- content_scripts/beastify.js
+++ content_scripts/beastify.js
@@ -1,31 +1,48 @@
-(() => {
-  if(window.hasRun) {
+(function() {
+  /**
+   * Check and set a global guard variable.
+   * If this content script is injected into the same page again,
+   * it will do nothing next time.
+   */
+  if (window.hasRun) {
     return;
   }
-
   window.hasRun = true;
 
-  const insertBeast = (beastURL) => {
+  /**
+   * Given a URL to a beast image, remove all existing beasts, then
+   * create and style an IMG node pointing to
+   * that image, then insert the node into the document.
+   */
+  function insertBeast(beastURL) {
     removeExistingBeasts();
-    const beastImage = document.createElement("img");
+    let beastImage = document.createElement("img");
     beastImage.setAttribute("src", beastURL);
     beastImage.style.height = "100vh";
     beastImage.className = "beastify-image";
     document.body.appendChild(beastImage);
   }
 
-  const removeExistingBeasts = () => {
-    const existingBeasts = document.querySelectorAll(".beastify-image");
-    for (const beasts of existingBeasts) {
-      beasts.remove();
+  /**
+   * Remove every beast from the page.
+   */
+  function removeExistingBeasts() {
+    let existingBeasts = document.querySelectorAll(".beastify-image");
+    for (let beast of existingBeasts) {
+      beast.remove();
     }
   }
 
+  /**
+   * Listen for messages from the background script.
+   * Call "beastify()" or "reset()".
+   */
   browser.runtime.onMessage.addListener((message) => {
-    if(message.command === "beastify") {
+    if (message.command === "beastify") {
       insertBeast(message.beastURL);
     } else if (message.command === "reset") {
       removeExistingBeasts();
     }
   });
+
 })();
--- popup/choose_beast.js
+++ popup/choose_beast.js
@@ -1,62 +1,101 @@
+/**
+ * CSS to hide everything on the page,
+ * except for elements that have the "beastify-image" class.
+ */
 const hidePage = `body > :not(.beastify-image) {
   display: none;
 }`;
 
-const listenForClicks = () => {
-  document.addEventListener("click", (e) => {
-    const beastNameToUrl = (beastName) => {
-      switch (beastName) {
-        case "Frog":
-          return browser.runtime.getURL("beasts/frog.jpg");
-        case "Snake":
-          return browser.runtime.getURL("beasts/snake.jpg");
-        case "Turtle":
-          return browser.runtime.getUrl("beasts/turtle.jpg");
-      }
-    };
+/**
+* Listen for clicks on the buttons, and send the appropriate message to
+* the content script in the page.
+*/
+function listenForClicks() {
+document.addEventListener("click", (e) => {
+/**
+* Given the name of a beast, get the URL to the corresponding image.
+*/
+function beastNameToURL(beastName) {
+switch (beastName) {
+case "Frog":
+return browser.runtime.getURL("beasts/frog.jpg");
+case "Snake":
+return browser.runtime.getURL("beasts/snake.jpg");
+case "Turtle":
+return browser.runtime.getURL("beasts/turtle.jpg");
+}
+}
 
-    const beastify = (tabs) => {
-      browser.tabs.insertCSS({ code: hidePage }).then(() => {
-        const url = beastNameToUrl(e.target.textContent);
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "beastify",
-          beastURL: url,
-        });
-      });
-    };
+/**
+* Insert the page-hiding CSS into the active tab,
+* then get the beast URL and
+* send a "beastify" message to the content script in the active tab.
+*/
+function beastify(tabs) {
+browser.tabs.insertCSS({ code: hidePage }).then(() => {
+const url = beastNameToURL(e.target.textContent);
+browser.tabs.sendMessage(tabs[0].id, {
+command: "beastify",
+beastURL: url
+});
+});
+}
 
-    const reset = (tabs) => {
-      browser.tabs.removeCSS({ code: hidePage }).then(() => {
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "reset",
-        });
-      });
-    };
+/**
+* Remove the page-hiding CSS from the active tab,
+* send a "reset" message to the content script in the active tab.
+*/
+function reset(tabs) {
+browser.tabs.removeCSS({ code: hidePage }).then(() => {
+browser.tabs.sendMessage(tabs[0].id, {
+command: "reset",
+});
+});
+}
 
-    const reportError = (error) => {
-      console.error(`Could not beastify: ${error}`);
-    };
+/**
+* Just log the error to the console.
+*/
+function reportError(error) {
+console.error(`Could not beastify: ${error}`);
+}
 
-    if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
-      return;
-    }
+/**
+* Get the active tab,
+* then call "beastify()" or "reset()" as appropriate.
+*/
+if (e.target.tagName !== "BUTTON" || !e.target.closest("#popup-content")) {
+// Ignore when click is not on a button within <div id="popup-content">.
+return;
+} 
+if (e.target.type === "reset") {
+browser.tabs.query({active: true, currentWindow: true})
+.then(reset)
+.catch(reportError);
+} else {
+browser.tabs.query({active: true, currentWindow: true})
+.then(beastify)
+.catch(reportError);
+}
+});
+}
 
-    if (e.target.type === "reset") {
-      browser.tabs
-        .query({ active: true, currentWindow: true })
-        .then(reset)
-        .catch(reportError);
-    }
-  });
-};
+/**
+* There was an error executing the script.
+* Display the popup's error message, and hide the normal UI.
+*/
+function reportExecuteScriptError(error) {
+document.querySelector("#popup-content").classList.add("hidden");
+document.querySelector("#error-content").classList.remove("hidden");
+console.error(`Failed to execute beastify content script: ${error.message}`);
+}
 
-const reportExecuteScriptError = (error) => {
-  document.querySelector("#popup-content").classList.add("hidden");
-  document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script ${error.message}`);
-};
-
+/**
+* When the popup loads, inject a content script into the active tab,
+* and add a click handler.
+* If we couldn't inject the script, handle the error.
+*/
 browser.tabs
-  .executeScript({ file: "/content_scripts/beastify.js" })
-  .then(listenForClicks)
-  .catch(reportExecuteScriptError);
+.executeScript({ file: "/content_scripts/beastify.js" })
+.then(listenForClicks)
+.catch(reportExecuteScriptError);
